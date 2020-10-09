042de2c4d58eb9cc86a16e7b063864ff6f7a6e80
Dima Voronoi <dvoron@ttu.ee>
2020-10-09 15:34:40 +0300

    Added working navigation bar with routerLink, filling contact tab with information. On image press in search bar we can see search results that should be filled with answers from back end ( API )

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', redirectTo: 'about-component', pathMatch: 'full'},
+  // {path: '', component: AppComponent},
+  {path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  // {path: 'home', redirectTo: 'about', pathMatch: 'full'}
+  // {path: '', redirectTo: '/about', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routing = RouterModule.forRoot(routes);
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,14 +3,22 @@ import {Component, Input, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WeatherComponent } from './weather/weather.component';
+// import { WeatherComponent } from './weather/weather.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent,
-    SearchBarComponent
+    // WeatherComponent,
+    SearchBarComponent,
+    NavBarComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
