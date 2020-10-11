c38bdcf62bf20e06ebdccd27e8ea9d7cded2c1ea
Dima Voronoi <dvoron@ttu.ee>
2020-10-12 01:23:52 +0300

    Front end can now receive and show responses about current weather and cases of corona virus for the chosen city/country

---
+++ front/src/app/corona.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {MessageService} from "./message.service";
+import {Observable, of} from "rxjs";
+import {Forecast} from "./forecast";
+import {catchError, tap} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoronaService {
+
+
+  private url: string = "http://localhost:8080/api/coronaViruses/country/";
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient, private messageService: MessageService ) { }
+
+
+  // getForecasts(): Observable<Forecast[]> {
+  //   return this.http.get<Forecast[]>(this.url).pipe(tap(_ => this.log('fetched forecasts')),
+  //     catchError(this.handleError<Forecast[]>('getForecasts', [])));
+  // }
+
+  getCorona(country: string): Observable<Forecast> {
+    const otherUrl = `${this.url}/${country}`;
+    return this.http.get<Forecast>(otherUrl).pipe(tap(_ => this.log(`fetched corona country=${country}`)))
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    }
+  }
+
+
+  private log(message: string) {
+    this.messageService.add(`CoronaService: ${message}`);
+  }
+}
+
+
--- front/src/app/search-bar/search-bar.component.ts
+++ front/src/app/search-bar/search-bar.component.ts
@@ -3,6 +3,8 @@ import {Forecast} from "../forecast";
 import {ActivatedRoute} from "@angular/router";
 import {Observable, Subscription} from "rxjs";
 import {ForecastServiceService} from "../forecast-service.service";
+import {JsonObject} from "@angular/compiler-cli/ngcc/src/packages/entry_point";
+import {CoronaService} from "../corona.service";
 
 @Component({
   selector: 'app-search-bar',
@@ -13,29 +15,74 @@ export class SearchBarComponent implements OnInit {
   // forecast: Forecast;
 
   show: boolean = false;
-  cityName: string = "";
-  weatherImage: string = "";
-  subscription = new Subscription();
-  forecastInfo: any = [{}];
+  // cityName: string = "";
+  // weatherImage: string = "";
+  ForecastSubscription = new Subscription();
+  coronaSubscription = new Subscription();
+  // forecastInfo: string;
   // xhr = new XMLHttpRequest();
+  countryName = "";
+  city = "";
+  weather = "";
+  temperature = "";
+  // lon = "";
+  // lat = "";
+// , this.lon = res['lon'], this.lat = res['lat']
+  wind = "";
+  pressure = "";
+  humidity = "";
+  suggestion = "";
+  totalCases = "";
+  recoveredCases = "";
+  totalDeaths = "";
+  currentCases = "";
 
 
 
   url = "http://localhost:8080/api/Forecast/city";
+  coronaUrl = "http://localhost:8080/api/coronaViruses/country/";
   onClick(value) {
     // this.show = true;
     // this.subscription.add(this.hs.getWeatherForecasts(this.url + value).subscribe(res => {console.log("Test if works");
     //   this.forecastInfo = res['data']}));
     // console.log(this.forecastInfo)
     this.show = true;
-    this.subscription.add(this.hs.getForecast(value).subscribe(res => {console.log("HOPE");
-    this.forecastInfo = res['data']}));
-    // this.forecastInfo = res['data'];
-    console.log(this.forecastInfo)
+    this.ForecastSubscription.add(this.hs.getForecast(value).subscribe(res => {console.log("RESPONSE INFO: " + JSON.stringify(res));
+    // this.forecastInfo = res['data']}));
+    this.countryName = res['countryName'], this.city = res['city'], this.weather = res['weather']
+    , this.temperature = res['temperature'], this.wind = res['wind'], this.pressure = res['pressure'], this.humidity = res['humidity']
+      , this.suggestion = res['suggestion']}));
+    // while (this.countryName == "") {
+    // }
+    setTimeout(() => {
+      this.coronaSubscription.add(this.coronaService.getCorona(this.countryName).subscribe(resp => {console.log(JSON.stringify(resp));
+      this.totalCases = resp['totalCases'], this.recoveredCases = resp['recoveredCases'], this.totalDeaths = resp['totalDeaths']
+      , this.currentCases = resp['currentCases']}))
+    }, 750);
+
+
+    // COuntryName
+    // totalCases
+    //recoveredCases
+    //totalDeaths
+    //currentCases
+
+
+    // console.log(this.countryName);
+    // console.log(this.city);
+    // console.log(this.weather);
+    // console.log(this.temperature);
+    // console.log(this.lon);
+    // console.log(this.lat);
+    // console.log(this.wind);
+    // console.log(this.pressure);
+    // console.log(this.humidity);
+    // console.log(this.suggestion);
   }
   constructor(
     private route: ActivatedRoute,
-    private hs: ForecastServiceService
+    private hs: ForecastServiceService,
+    private coronaService: CoronaService
     // private location: Location
   ) { }
 
